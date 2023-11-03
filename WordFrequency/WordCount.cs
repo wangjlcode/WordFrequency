e0e3b9a5928d8f90671579f103c40816d25b92a4
Jinglin Wang <JWang317@slb.com>
2023-11-03 15:10:09 +0800

    refactor

---
+++ WordFrequency/WordCount.cs
@@ -0,0 +1,24 @@
+﻿namespace WordFrequency
+{
+    public class WordCount
+    {
+        private string word;
+        private int count;
+
+        public WordCount(string w, int i)
+        {
+            this.word = w;
+            this.count = i;
+        }
+
+        public string Word
+        {
+            get { return this.word; }
+        }
+
+        public int Count
+        {
+            get { return this.count; }
+        }
+    }
+}
